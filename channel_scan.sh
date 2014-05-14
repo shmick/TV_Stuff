d4f7cce54612bb992f4f7f91f1f659ada8ef2bd2
shmick <github@warning.ca>
2014-05-13 22:32:08 -0400

    modified:   channel_scan.sh now supports -devid and -datalog

--- channel_scan.sh
+++ channel_scan.sh
@@ -3,13 +3,11 @@
 ######################################################################################
 #
 # channel_scan.sh 
-# v2014.05.13.r1
+# v2014.05.13.r2
 #
 # This script will scan a HDHomeRun for ATSC channels and print a formatted list
 #
-# Use the -csv option to print out CSV formatted results
-# Use the -datalog option to append the results to the DATALOG file defined below
-# Use the -noscan option to select a HDHR Unit and Tuner but not perform a scan
+# Use the --help option for additional runtime options
 #
 # A note about deciphering the results here:
 # http://www.silicondust.com/forum2/viewtopic.php?t=4474
@@ -18,13 +16,11 @@
 #
 ######################################################################################
 
+#Specify the full path to your hdhomerun_config binary if it's not found
 HDHRConfig=""
 
 MACOS="/usr/bin/hdhomerun_config"
 LINUX="/usr/local/bin/hdhomerun_config"
-
-Arg1=$1
-Arg2=$2
 
 if [ -x "$MACOS" ]
 then
@@ -42,18 +38,62 @@ echo ""
 exit
 fi
 
-if [[ "$Arg1" = "-datalog" && "$Arg2" = "" ]]
+if [ "$1" = "--help" ]
 then
 echo ""
-echo "You must specify a file ie: $0 $1 /path/to/datalog.txt"
+echo "Usage: $0 [option]"
+echo ""
+echo "Options are:"
+echo ""
+echo "-devid ID : Provide a device ID instead of using auto discovery"
+echo "-datalog /path/to/filename : Useful for scheduling scans and logging the data"
+echo ""
+echo "You may use both the -devid and -datalog options at the same time"
+echo ""
+echo "-noscan : Report only what device and tuner would be used without running a scan"
 echo ""
 exit
+fi
+
+Arg1=$1
+Arg2=$2
+Arg3=$3
+Arg4=$4
+
+# Exit if -datalog option is called but no file is specified
+if [[ "$Arg1" = "-datalog" && "$Arg2" = "" ]] ||  [[ "$Arg3" = "-datalog" && "$Arg4" = "" ]]
+then
+echo ""
+echo "You must specify a datalog file ie: $0 -datalog datalogfile"
+echo ""
+exit
+fi
+
+# Override the auto detect function by specifying a device ID
+# with the -devid option
+if [[ "$Arg1" = "-devid" && "$Arg2" = "" ]] || [[ "$Arg3" = "-devid" && "$Arg4" = "" ]]
+then
+echo ""
+echo "You must specify a device ID"
+echo ""
+exit
+elif [ "$Arg1" = "-devid" ]
+then
+Devices="$Arg2"
+elif [ "$Arg3" = "-devid" ]
+then
+Devices="$Arg4"
+else
+Devices=""
 fi
 
 DiscoverDevices () {
 # Attempt to discover HDHR devices on the LAN
 #
-Devices=`$HDHRConfig discover | awk '{print $3}'`
+if [ "$Devices" = "" ]
+then
+Devices=$($HDHRConfig discover | awk '{print $3}')
+fi
 
 # Exit if no device are found
 #
@@ -69,7 +109,7 @@ CheckTunerLockStatus () {
 	do
 		for Tuner in 0 1
 		do 
-		if [ $(eval $HDHRConfig $Unit get /tuner$Tuner/lockkey) = "none" ]
+		if [ $($HDHRConfig $Unit get /tuner$Tuner/lockkey) = "none" ]
 		then
 		ScanDev=$Unit
 		ScanTuner=$Tuner
@@ -101,14 +141,15 @@ exit
 fi
 }
 
+
 # Perform a tuner scan, outputting the results to $ScanResults
 #
 FullScan () {
 
 	echo ""
-	echo "Beginning scan on $ScanDev, tuner $ScanTuner at `date '+%D %T'`"
+	echo "Beginning scan on $ScanDev, tuner $ScanTuner at $(date '+%D %T')"
 	echo ""
-	ScanResults=`$HDHRConfig $ScanDev scan $ScanTuner \
+	ScanResults=$($HDHRConfig $ScanDev scan $ScanTuner \
 		    | tr "\n" " " \
 		    | tr "(" " " \
 		    | tr ")" " " \
@@ -120,12 +161,12 @@ FullScan () {
 		    -e 's/ss=//g' \
 		    -e 's/s.q=//g' \
 		    | awk -v OFS='\t' '{print $1,$2,$3,$4,$5,$6,"\t"$7,$8,"\t"$9,$10,"\t"$11,$12,"\t"$13,$14,"\t"$15,$16}' \
-		    | sort -n`
+		    | sort -n)
 # $1 through $6 are:
 # RF, Strnght, Quality, Symbol, Virtual, Name
 # $7 through $16 print up to 5 subchannels found
 
-        NumChannels=`wc -l <<< "$ScanResults"`
+        NumChannels=$(wc -l <<< "$ScanResults")
 	echo "${NumChannels// }" channels found
 }
 
@@ -134,16 +175,29 @@ CheckTunerLockStatus
 CheckNoScan
 FullScan
 
+LogOutput () {
+	timestamp=$(date "+%Y-%m-%d %H:%M")
+	echo "$ScanResults" | awk -v ts="$timestamp" '{OFS="\t" ; print ts,$1,$2,$3,$4,$5,$6}' | sed $'s/\\\t/,/g' >> $DataLog
+	}
+
+StdOutput () {
+	echo -e 'RF\tStrnght\tQuality\tSymbol\tVirtual\tName\t\tVirt#2\tName'
+	printf '%.0s-' {1..72}; echo
+	echo "$ScanResults"
+	}
+
 # Options are -csv or datalog 
 if [ "$1" = "-csv" ]
 	then
 	echo "$ScanResults" | sed $'s/\\\t/,/g'
-elif [ "$1" = "-datalog" ]
+elif [ "$Arg1" = "-datalog" ]
 	then
-	timestamp=`date "+%Y-%m-%d %H:%M"`
-	echo "$ScanResults" | awk -v ts="$timestamp" '{OFS="\t" ; print ts,$1,$2,$3,$4,$5,$6}' | sed $'s/\\\t/,/g' >> $Arg2
+	DataLog="$Arg2"
+	LogOutput
+elif [ "$Arg3" = "-datalog" ]
+	then
+	DataLog="$Arg4"
+	LogOutput
 else
-	echo -e 'RF\tStrnght\tQuality\tSymbol\tVirtual\tName\t\tVirt#2\tName'
-	printf '%.0s-' {1..72}; echo
-	echo "$ScanResults"
+	StdOutput
 fi
